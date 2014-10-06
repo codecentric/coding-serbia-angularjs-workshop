2fadb4adcaf3daab8897356674e9f115d4bb3a9b
Bastian Krol <bastian.krol@web.de>
2014-10-06 09:28:05 +0200

    introduction

---
+++ introduction/slide_config.js
@@ -0,0 +1,31 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'AngularJS Workshop',
+    subtitle: 'Introduction',
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    favIcon: '../favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  presenters: [{
+    name: 'Ben Ripkens',
+    company: 'codecentric AG',
+    gplus: 'http://plus.google.com/107194541655176085264',
+    twitter: '@benripkens',
+    www: 'http://bripkens.de',
+    github: 'http://github.com/bripkens'
+  }, {
+    name: 'Bastian Krol',
+    company: 'codecentric AG',
+    twitter: '@bastiankrol',
+    github: 'http://github.com/basti1302'
+  }]
+};
+
